4dcbe8c68c4fcb6f622d508fe85fce6e486c0814
cyb3rkevin <52473604+cyb3rkevin@users.noreply.github.com>
2020-12-24 14:17:36 -0500

    Update apibruter.py

--- apibruter.py
+++ apibruter.py
@@ -1,10 +1,10 @@
 #!/bin/python3
-#TryHackMe Advent of Cyber 2020 - Day 16 API challenge
+# TryHackMe Advent of Cyber 2020 - Day 16 API challenge
 
 import requests
 
 for api_key in range (1,100,2):
     print ("api key:" + str(api_key) )
-    #enter IP target below. The f' is used to format the api_key number into a string
+    # enter IP target below. The f' is used to format the api_key number into a string
     html = requests.get(f'http://<ENTER IP ADDRESS>/api/{api_key}')
     print (html.text)
