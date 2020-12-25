98830e29bb3dd15c910e77a11b3a7549b2e77826
cyb3rkevin <52473604+cyb3rkevin@users.noreply.github.com>
2020-12-24 21:11:03 -0500

    Update apibruter.sh

--- apibruter.sh
+++ apibruter.sh
@@ -2,15 +2,13 @@
 
 # 2020 Advent of Cyber Day 16 challenge (written in bash)
 
-# A directory named 'day16' was created to move wget files from showing up in current directory (i.e: /home/<USERNAME>) to retrieve the required output.
-
-# RECOMMEND to create the directory first before running the script
-
-# declare inital api number of 1 and increment by 2 to reach 100
+# declare inital api number of 1 and increment by 2 to 100 
 for number in $(seq 1 2 100);
 do
 	echo 'api number:' $number
 	wget -q  http://<TARGET IP>:8000/api/$number
-	mv $number ~/day16
-	cat ~/day16/$number | grep { 
+	# produce numbered file in current directory
+	cat $number | grep { 
+	# removing file after it gives output
+	rm $number
 done
