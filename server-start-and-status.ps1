01450ce2c03f98dd8f93c76158fc2b5661e34a24
cyb3rkevin <52473604+cyb3rkevin@users.noreply.github.com>
2020-12-28 15:44:32 -0500

    Delete server-start-and-status.ps1

--- server-start-and-status.ps1
+++
@@ -1,7 +0,0 @@
-<# Powershell Script I wrote to start Exchange Server 2016 for an email server class project #>
-
-<# start Exchange Server 2016 #>
-Start-Service *exchange*
-
-<# check all service components are in a running state #>
-Get-Service *exchange* | select -Property Status,Name,DisplayName,StartType
