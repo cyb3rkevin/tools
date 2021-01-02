902090abcfea38189221d25e20bdf2cb33117800
cyb3rkevin <52473604+cyb3rkevin@users.noreply.github.com>
2021-01-02 10:10:41 -0500

    Update exchange.ps1

--- exchange.ps1
+++ exchange.ps1
@@ -1,6 +1,6 @@
-<# Powershell Script I wrote to start Exchange Server 2016 for an email server class project #>
+<# Powershell script I wrote to start Exchange Server 2016 for a class project #>
 
-<# start Exchange Server 2016 #>
+<# start server #>
 Start-Service *exchange*
 
 <# check all service components are in a running state #>
